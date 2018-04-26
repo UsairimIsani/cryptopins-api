c612f4dfc8c0c2d6e057446e582cd03372a159c6
Usairim <usairim.isani@outlook.com>
2018-04-26 12:05:54 +0500

    Bitbucket Pipeline

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,8 @@
 import tracer from "tracer";
+import Axios from "axios";
 export const log = tracer.console({
   format: "{{message}}  - {{file}}:{{line}}"
 }).log;
+let axios = Axios.create({
+  baseURL: ""
+});
